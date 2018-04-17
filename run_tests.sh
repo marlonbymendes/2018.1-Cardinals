2e344cedfb807fcc924f9860e246b666d9bb8d3d
Marlon Mendes <marlonciriatico@gmail.com>
2018-04-16 23:12:10 -0300

    Setting up Flake8 in TravisCI

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# Django
 python manage.py makemigrations
-
 python manage.py migrate
 
+# Code test coverage
 coverage run --source='.' manage.py test
 coverage report
 
+# Integrate coverage with codecov
 codecov -t 4bbc492c-d908-4f02-8595-8a510df37ef3
+
+# Flake8
+flake8 .
